32668d02b919e952ddad6469afa31d67774f324f
Vincent Dansereau <dansereauvincent@gmail.com>
2019-03-23 15:21:38 -0400

    Add recipe ingredient data class

--- android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
+++ android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
@@ -4,6 +4,7 @@ import com.csgames.mixparadise.model.IngredientList
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.POST
 
 interface DrinkService {
     // TODO: get ingredients
@@ -12,4 +13,19 @@ interface DrinkService {
     fun getIngredients(): Call<IngredientList>
 
     // TODO: serve call
+    @Headers("Team: xargonauts")
+    @POST("serve")
+    fun submitRecipe(): Call<Recipe>
+ /*
+ “I see Jesus in every human being.
+ I say to myself, this is hungry Jesus, I must feed him.
+ This is sick Jesus. This one has leprosy or gangrene;
+ I must wash him and tend to him.
+
+ I serve because I love Jesus.”
+― Mother Teresa
+
+"I serve because I love CSGAMES!
+― Xargonauts
+  */
 }
