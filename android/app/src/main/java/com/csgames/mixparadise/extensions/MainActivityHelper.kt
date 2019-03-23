a26e8641278a591dc14af14c2d8f0050297103a7
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-03-23 15:58:07 -0400

    sending serve request

--- android/app/src/main/java/com/csgames/mixparadise/extensions/MainActivityHelper.kt
+++ android/app/src/main/java/com/csgames/mixparadise/extensions/MainActivityHelper.kt
@@ -1,14 +1,23 @@
 package com.csgames.mixparadise.extensions
 
 import android.os.Bundle
+import android.util.Log.d
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.doOnLayout
 import com.csgames.mixparadise.Blender
 import com.csgames.mixparadise.MainActivity
+import com.csgames.mixparadise.MixParadiseApplication
+import com.csgames.mixparadise.api.Api
 import com.csgames.mixparadise.ingredients.IngredientsBottomSheetDialogFragment
+import com.csgames.mixparadise.model.ServeItem
+import com.csgames.mixparadise.model.ServeResponse
 import kotlinx.android.synthetic.main.view_blender_with_table.*
+import kotlinx.android.synthetic.main.view_ingredient_item.view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 private const val ADD_INGREDIENTS_FRAGMENT_TAG = "ADD_INGREDIENTS_FRAGMENT_TAG"
 
@@ -45,6 +54,28 @@ fun MainActivity.setupListeners(blender: Blender, ingredientsDialog: Ingredients
         blender.empty()
         addIngredientsButton.visibility = View.VISIBLE
         serveButton.visibility = View.GONE
+
+        val serveItems: ArrayList<ServeItem> = ArrayList()
+        MixParadiseApplication.ingredients.juices
+            .filter { it.count > 0}
+            .forEach{ serveItems.add(ServeItem(it.id, it.count))}
+        MixParadiseApplication.ingredients.drinks
+            .filter { it.count > 0}
+            .forEach{ serveItems.add(ServeItem(it.id, it.count))}
+        MixParadiseApplication.ingredients.ingredients
+            .filter { it.count > 0}
+            .forEach{ serveItems.add(ServeItem(it.id, it.count))}
+        MixParadiseApplication.ingredients.alcohols
+            .filter { it.count > 0 }
+            .forEach{ serveItems.add(ServeItem(it.id, it.count))}
+
+        Api.drinkService.serveRecipe(serveItems.toList()).enqueue(object: Callback<ServeResponse> {
+            override fun onResponse(call: Call<ServeResponse>, response: Response<ServeResponse>) {
+                d("on serve raw", response.body().toString())
+            }
+            override fun onFailure(call: Call<ServeResponse>, t: Throwable) {
+            }
+        })
     }
 
     addIngredientsButton.setOnClickListener {
