5859cf33395a5cb69e0dc070440146489db6f9cc
Jeremie Bigras-Dunberry <bigjerbd@gmail.com>
2019-03-23 16:38:28 -0400

    api with encryption started WIP

--- android/app/src/main/java/com/csgames/mixparadise/api/Api.kt
+++ android/app/src/main/java/com/csgames/mixparadise/api/Api.kt
@@ -6,14 +6,45 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import android.provider.SyncStateContract.Helpers.update
+import java.io.UnsupportedEncodingException
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 object Api {
     private lateinit var retrofit: Retrofit
+    private val key = "tv8PDnId7ylIwGEQ5naooq3wnL205RNR";
 
     fun init(context: Context) {
         val okHttpClient = OkHttpClient.Builder()
             .cache(Cache(File(context.cacheDir, "http-cache"), 10 * 1024 * 1024))
             .build()
+
+        //added interceptor so that all query have a specific header
+        val interceptor = Interceptor { chain ->
+
+            // TODO :: do the proper sha1 encryption
+            //val timestamp = LocalDateTime.now(ZoneOffset.UTC).toEpochSecond())
+            val autorization : String = "csgames19-"
+
+            val request = chain.request()
+                .newBuilder()
+                .addHeader("Team", "McBrooke")
+                //.addHeader()
+                .build()
+
+
+            chain.proceed(request)
+        }
 
         retrofit = Retrofit.Builder()
             .baseUrl("https://mirego-csgames19.herokuapp.com")
@@ -25,4 +56,27 @@ object Api {
     val drinkService: DrinkService by lazy { retrofit.create(DrinkService::class.java) }
 
 
+
+    // do the ingredients api call asynchronously
+    fun ingredients() {
+        val call = Api.drinkService.ingredients()
+        call.enqueue(object : Callback<IngredientResponse.Result> {
+
+            override fun onFailure(call: Call<IngredientResponse.Result>?, t: Throwable?) {
+                throw error("an Error Has Occured")
+            }
+
+            override fun onResponse(call: Call<IngredientResponse.Result>?,
+                                    response: Response<IngredientResponse.Result>?) {
+                return print(response!!.body()!!);
+            }
+        })
+    }
+
+    //TODO :: add an other async api call for server
+
+
+
 }
+
+
--- android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
+++ android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
@@ -7,24 +7,9 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
 
-/*
-val ingred = Api.drinkService.ingredients()
-        ingred.enqueue(object : Callback<IngredientResponse.Result> {
-            override fun onFailure(call: Call<IngredientResponse.Result>?, t: Throwable?) {
-                print("aaa")
-            }
-
-            override fun onResponse(call: Call<IngredientResponse.Result>?,
-                                    response: Response<IngredientResponse.Result>?) {
-                print(response!!.body()!!.drinks.get(0).color)
-            }
-        })
- */
 
 interface DrinkService {
-    @Headers(
-        "Team: McBrooke"
-    )
+
     @GET("/ingredient")
     fun ingredients(): Call<IngredientResponse.Result>
 
@@ -32,7 +17,7 @@ interface DrinkService {
     fun serve(id: String, qty: Int): Call<ServeResponse.Result>
 }
 
-// TODO : move to an other file
+// TODO : move these classes to an other files
 object IngredientResponse {
     data class Result(
         val juices: List<Liquid>,
@@ -76,31 +61,4 @@ object ServeResponse {
         val volume: Int,
         val strength: Int
     )
-
-
-}
-
-
-
-
-// NOTE :: will be used if we can reach this point
-fun<T> Call<T>.enqueue(callback: CallBackKt<T>.() -> Unit) {
-    val callBackKt = CallBackKt<T>()
-    callback.invoke(callBackKt)
-    this.enqueue(callBackKt)
-}
-
-class CallBackKt<T>: Callback<T> {
-
-    var onResponse: ((Response<T>) -> Unit)? = null
-    var onFailure: ((t: Throwable?) -> Unit)? = null
-
-    override fun onFailure(call: Call<T>, t: Throwable) {
-        onFailure?.invoke(t)
-    }
-
-    override fun onResponse(call: Call<T>, response: Response<T>) {
-        onResponse?.invoke(response)
-    }
-
 }
