6d8826b6f39c0e49579a65a6e33e5af27021d487
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-03-23 15:08:03 -0400

    convert Ingredient to a dataclass

--- android/app/src/main/java/com/csgames/mixparadise/model/Ingredient.kt
+++ android/app/src/main/java/com/csgames/mixparadise/model/Ingredient.kt
@@ -1,10 +1,10 @@
 package com.csgames.mixparadise.model
 
-class Ingredient {
-    var id: String = ""
-    var label: String = ""
-    var type: String = ""
-    var weight: Double = 0.0
-    var imageUrl: String = ""
+data class Ingredient (
+    var id: String = "",
+    var label: String = "",
+    var type: String = "",
+    var weight: Double = 0.0,
+    var imageUrl: String = "",
     var sprites: List<String>? = null
-}
+)
