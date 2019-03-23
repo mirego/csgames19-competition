89866b6837ab83e182de3f335d6b1dfc0c284a73
marc <matherrien@hotmail.ca>
2019-03-23 14:00:28 -0400

    complete ingredient model

--- android/app/src/main/java/com/csgames/mixparadise/model/Ingredient.kt
+++ android/app/src/main/java/com/csgames/mixparadise/model/Ingredient.kt
@@ -3,4 +3,8 @@ package com.csgames.mixparadise.model
 class Ingredient {
     var id: String = ""
     var label: String = ""
+    var type: String = ""
+    var weight: Double = 0.0
+    var imageUrl: String = ""
+    var sprites: List<String>? = null
 }
