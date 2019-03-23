e752d0efa69731fa996fdef3197217439214049f
peakyDicers <ian.chui@hotmail.com>
2019-03-23 15:53:24 -0400

    added set and gets

--- android/app/src/main/java/com/csgames/brock/Ingredients.java
+++ android/app/src/main/java/com/csgames/brock/Ingredients.java
@@ -1,5 +1,3 @@
-package com.csgames.brock;
-
 import java.util.ArrayList;
 
 public class Ingredients {
@@ -11,4 +9,34 @@ public class Ingredients {
     public ArrayList<Drink> getJuices() {
         return juices;
     }
+    
+    //getters and setters
+
+	public ArrayList<Drink> getDrinks() {
+		return drinks;
+	}
+
+	public ArrayList<Drink> getIngredients() {
+		return ingredients;
+	}
+
+	public ArrayList<Drink> getAlcohols() {
+		return alcohols;
+	}
+
+	public void setJuices(ArrayList<Drink> juices) {
+		this.juices = juices;
+	}
+
+	public void setDrinks(ArrayList<Drink> drinks) {
+		this.drinks = drinks;
+	}
+
+	public void setIngredients(ArrayList<Drink> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public void setAlcohols(ArrayList<Drink> alcohols) {
+		this.alcohols = alcohols;
+	}
 }
