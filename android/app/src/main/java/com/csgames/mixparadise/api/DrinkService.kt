f1431bbaf3da7e676cd3990381772b0341319ac1
Francis <francislord95@hotmail.com>
2019-03-23 16:12:04 -0400

    WIP

--- android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
+++ android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
@@ -1,7 +1,26 @@
 package com.csgames.mixparadise.api
 
+import android.database.Observable
+import android.util.Log
+import com.csgames.mixparadise.model.StackedIngredient
+import retrofit2.http.GET
+//import jdk.nashorn.internal.runtime.ScriptingFunctions.readLine
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import retrofit2.http.Query
+import retrofit2.Call
+import retrofit2.http.Headers
+
+
 interface DrinkService {
-    // TODO: get ingredients
+
+    @Headers("Team:UQO")
+    @GET("Ingredients")
+    fun getIngredient(): Call<ArrayList<StackedIngredient>>
+
 
     // TODO: serve call
 }
--- android/app/src/main/java/com/csgames/mixparadise/ingredients/IngredientsBottomSheetDialogFragment.kt
+++ android/app/src/main/java/com/csgames/mixparadise/ingredients/IngredientsBottomSheetDialogFragment.kt
@@ -1,12 +1,18 @@
 package com.csgames.mixparadise.ingredients
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.csgames.mixparadise.R
+import com.csgames.mixparadise.api.Api
 import com.csgames.mixparadise.extensions.setImmersiveMode
+import com.csgames.mixparadise.model.StackedIngredient
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.view_ingredients_dialog.view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 typealias IngredientSelectedListener = (
     id: String
@@ -49,5 +55,28 @@ class IngredientsBottomSheetDialogFragment : BottomSheetDialogFragment() {
             BottomSheetBehavior.from(bottomSheet).isHideable = false
             bottomSheet.layoutParams?.height = ViewGroup.LayoutParams.MATCH_PARENT
         }
+
+        var test = Api.drinkService.getIngredient()
+        test.enqueue(object : Callback<ArrayList<StackedIngredient>> {
+            override fun onFailure(call: Call<ArrayList<StackedIngredient>>?, t: Throwable?) {
+                Log.e("error", t?.message);
+            }
+
+            override fun onResponse(call: Call<ArrayList<StackedIngredient>>?, response: Response<ArrayList<StackedIngredient>>?) {
+                Log.d("Test", response?.code().toString())
+            }
+        })
     }
+
+fun <T> callback(fn: (Throwable?, Response<T>?) -> Unit): Callback<T> {
+    return object : Callback<T> {
+        override fun onResponse(call: Call<T>, response: retrofit2.Response<T>) = fn(null, response)
+        override fun onFailure(call: Call<T>, t: Throwable) = fn(t, null)
+    }
+}
+
+    fun testCallback(test : ArrayList<StackedIngredient>) {
+
+    }
+
 }
