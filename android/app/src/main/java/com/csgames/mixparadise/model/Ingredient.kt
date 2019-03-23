6c02b090a5c2451724787646e49249c15111f3e6
marc <matherrien@hotmail.ca>
2019-03-23 13:59:04 -0400

    add models

---
+++ android/app/src/main/java/com/csgames/mixparadise/model/Ingredient.kt
@@ -0,0 +1,6 @@
+package com.csgames.mixparadise.model
+
+class Ingredient {
+    var id: String = ""
+    var label: String = ""
+}
---
+++ android/app/src/main/java/com/csgames/mixparadise/model/Juice.kt
@@ -0,0 +1,10 @@
+package com.csgames.mixparadise.model
+
+class Juice {
+    var id: String = ""
+    var label: String = ""
+    var color: String = ""
+    var type: String = ""
+    var opacity: Double = 0.0
+    var imageUrl: String = ""
+}
