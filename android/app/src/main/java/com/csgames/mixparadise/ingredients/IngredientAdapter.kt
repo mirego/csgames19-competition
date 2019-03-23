10e6d45ede16a4abbac11f11b37f8fc12329f2d3
Erin <erin.benderoff@gmail.com>
2019-03-23 15:40:07 -0400

    created Ingredient viewholder and adapter, added remaining properties to model

---
+++ android/app/src/main/java/com/csgames/mixparadise/ingredients/IngredientAdapter.kt
@@ -0,0 +1,26 @@
+package com.csgames.mixparadise.ingredients
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.csgames.mixparadise.model.StackedIngredient
+
+class IngredientAdapter(private val context: Context?, private val myDataset: Array<StackedIngredient>) :
+    RecyclerView.Adapter<IngredientViewHolder>() {
+
+    private val inflater: LayoutInflater
+            = context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder {
+        return IngredientViewHolder(inflater, parent, this.context)
+    }
+
+    override fun onBindViewHolder(holder: IngredientViewHolder, position: Int) {
+        val ingredient: StackedIngredient = myDataset[position]
+        holder.bind(ingredient)
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = myDataset.size
+}
---
+++ android/app/src/main/java/com/csgames/mixparadise/ingredients/IngredientViewHolder.kt
@@ -0,0 +1,31 @@
+package com.csgames.mixparadise.ingredients
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.csgames.mixparadise.model.StackedIngredient
+import com.bumptech.glide.Glide
+import com.csgames.mixparadise.R
+
+
+class IngredientViewHolder(inflater: LayoutInflater, parent: ViewGroup, private val context: Context?) :
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.view_ingredient_item, parent, false)) {
+    private var countView: TextView? = null
+    private var titleView: TextView? = null
+    private var imageView: ImageView? = null
+
+    init {
+        countView = itemView.findViewById(R.id.count)
+        titleView = itemView.findViewById(R.id.title)
+        imageView = itemView.findViewById(R.id.image_view)
+    }
+
+    fun bind(ingredient: StackedIngredient) {
+        countView?.text = "2"
+        titleView?.text = ingredient.label
+        Glide.with(context!!).load(ingredient.imageUrl).into(imageView!!)
+    }
+}
