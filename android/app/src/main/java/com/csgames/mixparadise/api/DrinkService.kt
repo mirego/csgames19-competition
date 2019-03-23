ca2f36fa1cd54ef21ced7f76107dc9ffeef8b738
Wynand Badenhorst <wynandbadenhorst@hotmail.com>
2019-03-23 15:02:39 -0500

    added some classes

--- android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
+++ android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
@@ -3,8 +3,10 @@ package com.csgames.mixparadise.api
 import okhttp3.Cache
 import okhttp3.Call
 import okhttp3.OkHttpClient
+import okhttp3.RequestBody
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -18,13 +20,29 @@ interface DrinkService {
     )
 
     @GET("/ingredients")
-    fun getIngredients():Call
+    fun getIngredients(@Body request: RequestBody):Call
 
     @POST("/serve")
     fun serveDrink(ingr: HashMap<String,Int>):Call
 }
 
-class Ingr(
+data class Ingr(
     var id: String,
     var qty: Integer
+)
+
+data class DrinkResponse(
+    var rating: Rating,
+    var review: Review
+)
+
+data class Rating(
+    var note: Integer,
+    var comment: String
+)
+
+data class Review(
+    var taste: Integer,
+    var volume: Integer,
+    var strength: Integer
 )
