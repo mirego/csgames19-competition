a222db174d8d6738d8783810d7cb08e54fa004eb
Wynand Badenhorst <wynandbadenhorst@hotmail.com>
2019-03-23 14:05:27 -0500

    added get request for ingredients

--- android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
+++ android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
@@ -1,7 +1,30 @@
 package com.csgames.mixparadise.api
 
-interface DrinkService {
-    // TODO: get ingredients
+import okhttp3.Cache
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import java.io.File
 
-    // TODO: serve call
+// TODO: Write this stuff
+
+interface DrinkService {
+    @Headers(
+        "Team:friendSHIP"
+    )
+
+    @GET("/ingredients")
+    fun getIngredients():Call
+
+    @POST("/serve")
+    fun serveDrink(ingr: HashMap<String,Int>):Call
 }
+
+class Ingr(
+    var id: String,
+    var qty: Integer
+)
